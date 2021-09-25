75dcf71bf1befeadc8a99a7d4cb66dcea85c5f69
stanleymok <stanleymwork@gmail.com>
2021-09-25 18:07:05 +0800

    feat: finish! POC

--- src/db_manager.py
+++ src/db_manager.py
@@ -13,42 +13,64 @@ class DBM:
                         AGE            INT     NOT NULL,
                         GENDER         CHAR(1)     NOT NULL)
                         ;''')
-                print ("PATIENTS Table created successfully")
+                print ("PATIENTS Table opened/created successfully")
                 self.conn.execute('''CREATE TABLE IF NOT EXISTS DOCTORS
                         (DOC_ID        VARCHAR(10) PRIMARY KEY NOT NULL,
                         NAME           VARCHAR(50)    NOT NULL)
                         ;''')
-                print("DOCTORS Table created successfully")
+                print("DOCTORS Table opened/created successfully")
                 self.conn.execute('''CREATE TABLE IF NOT EXISTS APPOINTMENTS
-                        (APPT_ID       VARCHAR(15) PRIMARY KEY NOT NULL,
-                        DATE           CHAR(8) NOT NULL,
-                        TIME           CHAR(8) NOT NULL,
-                        DOC_ID        VARCHAR(10),
-                        PATIENT_ID    VARCHAR(10) NOT NULL,
-                        PATIENT_NAME           VARCHAR(50) NOT NULL,
-                        PATIENT_AGE            INT     NOT NULL,
-                        PATIENT_GENDER         CHAR(1)     NOT NULL)
+                        (APPT_ID        VARCHAR(15) PRIMARY KEY NOT NULL,
+                        DATE            CHAR(8) NOT NULL,
+                        TIME            CHAR(8) NOT NULL,
+                        DOC_ID          VARCHAR(10) NOT NULL,
+                        PATIENT_ID      VARCHAR(10),
+                        PATIENT_NAME    VARCHAR(50) NOT NULL,
+                        PATIENT_AGE     INT,
+                        PATIENT_GENDER  CHAR(1))
                         ;''')
-                print("APPOINTMENTS Table created successfully")
+                print("APPOINTMENTS Table opened/created successfully")
 
         def add_patient(self, id, name, age, gender):
                 self.conn.execute("INSERT OR IGNORE INTO PATIENTS (PATIENT_ID,NAME,AGE,GENDER) values (?, ?, ?, ?)",
                         (id, name, age, gender))
                 self.conn.commit()
+
         def add_doctor(self, id, name):
                 self.conn.execute("INSERT OR IGNORE INTO DOCTORS (DOC_ID,NAME) values (?, ?)",
                         (id, name))
                 self.conn.commit()
+
         def add_appt(self, appt_id, date, time, d_id, p_id, p_name, p_age, p_gender):
                 self.conn.execute("INSERT OR IGNORE INTO APPOINTMENTS (APPT_ID,DATE,TIME,DOC_ID,PATIENT_ID,PATIENT_NAME,PATIENT_AGE,PATIENT_GENDER) \
-                        values (?, ?, ?, ?, ?, ?, ?, ?)",
-                        (appt_id, date, time, d_id, p_id, p_name, p_age, p_gender))
+                                        values (?, ?, ?, ?, ?, ?, ?, ?)", (appt_id, date, time, d_id, p_id, p_name, p_age, p_gender))
                 self.conn.commit()
 
         def get_appts(self, doc_id, date, time):
                 cursor = self.conn.execute(f"SELECT * FROM APPOINTMENTS WHERE DOC_ID = \"{doc_id}\" AND DATE = \"{date}\" AND TIME = \"{time}\";")
                 for row in cursor:
                         print(row)
+                                
+        def fix_appt(self, patient_name, doc_id, date, time):
+                cursor = self.conn.execute(f"SELECT MAX(APPT_ID) FROM APPOINTMENTS")   
+                appt_id = cursor.fetchone()[0]
+                # inc id by one
+                appt_id = appt_id[0] + str(int(appt_id[1:]) + 1)
+                print(appt_id)
+                self.conn.execute("INSERT INTO APPOINTMENTS (APPT_ID, PATIENT_NAME, DOC_ID, DATE, TIME) \
+                                        values (?, ?, ?, ?, ?)", (appt_id, patient_name, doc_id, date, time))
+                self.conn.commit()
+
+        def del_appt(self, patient_name, doc_id, date, time):
+                cursor = self.conn.execute(f"SELECT MAX(APPT_ID) FROM APPOINTMENTS")   
+                appt_id = cursor.fetchone()[0]
+                # inc id by one
+                appt_id = appt_id[0] + str(int(appt_id[1:]) + 1)
+                print(appt_id)
+                self.conn.execute(f"DELETE FROM APPOINTMENTS WHERE PATIENT_NAME = \"{patient_name}\" AND DOC_ID = \
+                                        \"{doc_id}\" AND  DATE = \"{date}\" AND TIME = \"{time}\";")
+                self.conn.commit()
+
 
         def close(self):
                 self.conn.close()
--- src/main.py
+++ src/main.py
@@ -3,75 +3,111 @@ import os
 from db_manager import DBM
 
 HELP_TEXTS = ["", "==================== HELP ========================", 
-                "Read csv: type <read_csv> then type <file_name> in inputs folder"
-                "Get appts: type <get_appt> then type <doc_id><space><date_time>",
-                "Add appt: type <add_appt> then type <patient_id><space><doct_id><space><date_time>",
-                "Del appt: type <del_appt> then type <patient_id><space><doct_id><space><date_time>",
+                "Read csv: type <read_csv>",
+                "Get appts: type <get_appt>",
+                "Fix appt: type <fix_appt>",
+                "Del appt: type <del_appt>",
                 "Exit: type <exit>",
                 "Help: type <help> or any string unspecified above"] 
 CWD = os.getcwd()
 PATH_TO_INPUTS = "/../inputs/"
 
 
-def read_file(file_name, dbm):
-    f = open(CWD + PATH_TO_INPUTS + file_name,"r")
-    lines = f.readlines()[1:]
-    # data processing
-    for line in lines:
-        print(line)
-        data = line.split(", ")
-        if len(data) < 8: 
-            continue
-        doc_id = data[0]
-        doc_name = data[1]
-        patient_id = data[2]
-        patient_name = data[3]
-        patient_age = data[4]
-        patient_gender = data[5]
-        appt_id = data[6]
-        date_time = data[7]
-        date_time = date_time.split()
-        date = date_time[0]
-        time = date_time[1]
-        # add to db
-        dbm.add_patient(patient_id, patient_name, patient_age, patient_gender)
-        dbm.add_doctor(doc_id, doc_name)
-        dbm.add_appt(appt_id, date, time, doc_id, patient_id, patient_name, patient_age, patient_gender)
+class Server:
+    def __init__(self):
+        self.dbm = DBM()
+        self.dbm.create_tables()
 
+    def print_help(self):
+        for help_text in HELP_TEXTS:
+            print(help_text) 
 
-def get_doc_appts(doc_id, date, time):
-    dbm.get_appts(doc_id, date, time)
+    def read_file(self):
+        print("Input file name: ")
+        file_name = input()
+        f = open(CWD + PATH_TO_INPUTS + file_name,"r")
+        lines = f.readlines()[1:]
+        # data processing
+        for line in lines:
+            data = line.split(", ")
+            if len(data) < 8: 
+                continue
+            doc_id = data[0]
+            doc_name = data[1]
+            patient_id = data[2]
+            patient_name = data[3]
+            patient_age = data[4]
+            patient_gender = data[5]
+            appt_id = data[6]
+            date_time = data[7]
+            date_time = date_time.split()
+            date = date_time[0]
+            time = date_time[1]
+            # add to db
+            self.dbm.add_patient(patient_id, patient_name, patient_age, patient_gender)
+            self.dbm.add_doctor(doc_id, doc_name)
+            self.dbm.add_appt(appt_id, date, time, doc_id, patient_id, patient_name, patient_age, patient_gender)
+        print("Read CSV successfully.")
+        self.print_help()
 
+    def get_doc_appts(self):
+        print("Input doc_id:")
+        doc_id = input()
+        print("Input date: format <dd><mm><yyyy>")
+        date = input()
+        print("Input time: format <hh>:<mm>:<ss>")
+        time = input()
+        self.dbm.get_appts(doc_id, date, time)
+        print("Select statement executed successfully.")
+        self.print_help()
+
+    def fix_appt(self):
+        print("Input patient_name:")
+        patient_name = input()
+        print("Doctor's id:")
+        doc_id = input()
+        print("Input date: format <dd><mm><yyyy>")
+        date = input()
+        print("Input time: format <hh>:<mm>:<ss>")
+        time = input()
+        self.dbm.fix_appt(patient_name, doc_id, date, time)
+        print("Insert statement executed successfully.")
+        self.print_help()
+
+    def del_appt(self):
+        print("Input patient_name:")
+        patient_name = input()
+        print("Doctor's id:")
+        doc_id = input()
+        print("Input date: format <dd><mm><yyyy>")
+        date = input()
+        print("Input time: format <hh>:<mm>:<ss>")
+        time = input()
+        self.dbm.del_appt(patient_name, doc_id, date, time)
+        print("============================================")
+        print("Delete statement executed successfully.")
+        self.print_help()
+
+    def run(self):
+        print("Type <help> for help")
+        for line in sys.stdin:
+            line = line.rstrip()
+            if line == "read_csv":
+                self.read_file()
+            elif line == "get_appt":
+                self.get_doc_appts()
+            elif line == "fix_appt": # patient wont know his ID, and doc's ID, hence never include
+                self.fix_appt()
+            elif line =="del_appt":
+                self.del_appt()
+            elif line == "exit":
+                break
+            elif line == "help":
+                self.print_help()
+            else:
+                self.print_help()
+        self.dbm.close()
 
 if __name__ == "__main__":
-    dbm = DBM()
-    dbm.create_tables()
-
-    print("Type <help> for help")
-    for line in sys.stdin:
-        line = line.rstrip()
-        if line == "read_csv":
-            print("Input file name: ")
-            read_file(input(), dbm)
-        elif line == "get_appt":
-            print("Input doc_id:")
-            doc_id = input()
-            print("Input date: format <dd><mm><yyyy>")
-            date = input()
-            print("Input time: format <hh>:<mm>:<ss>")
-            time = input()
-            get_doc_appts(doc_id, date, time)
-        elif line == "add_appt":
-            pass
-        elif line =="del_appt":
-            pass
-        elif line == "exit":
-            break
-        elif line == "help":
-            for help_text in HELP_TEXTS:
-                print(help_text) 
-        else:
-            for help_text in HELP_TEXTS:
-                print(help_text) 
-
-    dbm.close()
+    server = Server()
+    server.run()
